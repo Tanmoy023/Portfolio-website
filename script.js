057b9eabcf1d6770688e90265016b6fe84559534
Tanmoy Patra <tanmoypatra369@gmail.com>
2026-04-20 16:55:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed("#element", {
+    strings: [
+        "Full Stack Python Developer.",
+        "Frontend and Backend Integration.",
+        "Proficient in Web Development.",
+        "Skilled in API Development.",
+        "Developed AI-Powered Web Applications.",
+        "Experienced in Backend Development.",
+        "Developed Secure Authentication Systems.",
+        "Created Responsive and Modern UI.",
+        "Full Stack Python Developer.",
+    ],
+    typeSpeed: 30,
+});
+
+
+
+function showMessage(message) {
+    alert(message)
+}
